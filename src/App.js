1cb8ff79496fc081ab73c74f256a3040f02068b5
Armen Marjinyan <armenmarjinyan@MacBook-Pro-Armen.local>
2022-07-20 22:08:09 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Book from './pages/Book'
+class App extends React.Component {
+  state = {
+    books: []
+  }
+  componentDidMount() {
+    fetch("data/books.json")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ books: data.items })
+      })
+  }
+  render() {
+    const {books} = this.state
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return <div id="main">
+      <h1>Welcome to Book Store</h1>
+      <div id="books">
+        {
+          books.map((elm) => {
+            return <Book 
+                      id={elm.id}
+                      title={elm.title}
+                      author={elm.author}
+                      pic={elm.pic}
+                   />
+          })
+        }
+      </div>
+
     </div>
-  );
+  }
 }
-
-export default App;
+export default App
